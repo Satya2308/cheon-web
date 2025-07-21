068baef67f91011b60d42076940376c7db687ec2
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 21:42:58 +0700

    refactor: move login page to folder and add validateLogin function

---
+++ app/routes/_auth.login/data.ts
@@ -0,0 +1,11 @@
+import { flatten, getRaw } from "~/helpers/zod"
+import { z } from "zod"
+
+export async function validateLogin(data: FormData) {
+	const raw = getRaw(data)
+	const schema = z.object({
+		phone: z.string(),
+		password: z.string()
+	})
+	return await schema.safeParseAsync(raw).then(res => flatten(res))
+}
