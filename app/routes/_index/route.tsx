b9c783633d08e6d2111aafffa29960646aa8fdf2
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 21:42:19 +0700

    refactor: move home page to folder

---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "@remix-run/react";
+
+export async function loader(){
+  return redirect("/admin/teachers")
+}
+
+export default function Index() {
+  return (
+    <div>Welcome to BBK</div>
+  );
+}
