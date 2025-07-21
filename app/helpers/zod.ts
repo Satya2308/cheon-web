166f6001f4d48837200de79549b66caa3e8eb201
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 21:40:35 +0700

    feat: add getRaw and flatten functions

---
+++ app/helpers/zod.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export function getRaw(data: FormData, allowMultiple: boolean = false) {
+  const raw: { [key: string]: any } = {};
+  for (const key of data.keys()) {
+    const val = allowMultiple ? data.getAll(key) : data.get(key);
+
+    if (allowMultiple && Array.isArray(val)) {
+      if (val.length === 0) continue;
+      raw[key] = val
+        .map((item) =>
+          item instanceof File && item.size === 0 ? undefined : item
+        )
+        .filter((item) => item !== undefined);
+    } else {
+      if (val === undefined) continue;
+      const notFile = !(val instanceof File);
+      const notEmptyFile = val instanceof File && val.size > 0;
+      if (notFile || notEmptyFile) raw[key] = val;
+    }
+  }
+  return raw;
+}
+
+export function flatten<Input, Output>(
+	res: z.SafeParseReturnType<Input, Output>
+) {
+	const ok = res.success
+	if (ok) return { ok, data: res.data, error: null }
+	return { ok, data: null, error: res.error.flatten().fieldErrors }
+}
